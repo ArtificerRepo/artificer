63c2229e8cca838ef63e86fe34ec89d0383ee6aa
Kurt Stam <kstam@redhat.com>
2012-04-18 17:39:23 -0400

    SRAMP-6 adding test

---
+++ s-ramp-repository/src/test/java/org/guvnor/sramp/repository/jcr/XsdModelTest.java
@@ -0,0 +1,17 @@
+package org.guvnor.sramp.repository.jcr;
+
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:kurt.stam@gmail.com">Kurt Stam</a>
+ * @version $Revision: 1 $
+ */
+public class XsdModelTest {
+    
+    @Test
+    public void unmarshallCoreModelWorkspace() throws Exception {
+       
+    }
+   
+    
+}
