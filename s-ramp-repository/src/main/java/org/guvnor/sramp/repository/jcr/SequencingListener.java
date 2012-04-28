4a8846ab476a4906abd2f557b318b2ba599814c2
Kurt Stam <kstam@redhat.com>
2012-04-28 16:02:30 -0400

    SRAMP-5 adding JCR implementatation

--- s-ramp-repository/src/main/java/org/guvnor/sramp/repository/jcr/SequencingListener.java
+++ s-ramp-repository/src/main/java/org/guvnor/sramp/repository/jcr/SequencingListener.java
@@ -2,15 +2,21 @@ package org.guvnor.sramp.repository.jcr;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
+import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
 import javax.jcr.observation.Event;
 import javax.jcr.observation.EventIterator;
 import javax.jcr.observation.EventListener;
 
+import org.guvnor.sramp.repository.UnsupportedFiletypeException;
+import org.modeshape.jcr.JcrSession;
+
 public class SequencingListener implements EventListener {
 
-    public ConcurrentHashMap<String, String> sequencedNodePaths = new ConcurrentHashMap<String, String>();
-    public ConcurrentHashMap<String, CountDownLatch> waitingLatches = new ConcurrentHashMap<String, CountDownLatch>();
+    private ConcurrentHashMap<String, CountDownLatch> waitingLatches = new ConcurrentHashMap<String, CountDownLatch>();
   
     @Override
     public void onEvent( EventIterator events ) {
@@ -19,12 +25,31 @@ public class SequencingListener implements EventListener {
                 Event event = (Event)events.nextEvent();
                 String nodePath = event.getPath();
                 System.out.println("Received created sequenced node event at: " + nodePath);
-                sequencedNodePaths.put(nodePath, nodePath);
-                // signal the node is available
                 waitingLatches.get(nodePath).countDown();
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
     }
+    
+    public void addWaitingLatch(String artifactFileName) throws UnsupportedFiletypeException {
+        String derivedNodePath = MapToJCRPath.getDerivedArtifactPath(artifactFileName);
+        waitingLatches.putIfAbsent(derivedNodePath, new CountDownLatch(1));
+    }
+    
+    public void waitForLatch(String artifactFileName) throws InterruptedException, UnsupportedFiletypeException {
+        String derivedNodePath = MapToJCRPath.getDerivedArtifactPath(artifactFileName);
+        waitingLatches.get(derivedNodePath).await(60, TimeUnit.SECONDS);
+    }
+    
+    public Node getDerivedNode(String artifactFileName, JcrSession session) throws PathNotFoundException, RepositoryException, UnsupportedFiletypeException {
+        String derivedNodePath = MapToJCRPath.getDerivedArtifactPath(artifactFileName);
+        Node derivedNode = session.getNode(derivedNodePath);
+        
+        return derivedNode;
+    }
+    
+    
+    
+    
 }
