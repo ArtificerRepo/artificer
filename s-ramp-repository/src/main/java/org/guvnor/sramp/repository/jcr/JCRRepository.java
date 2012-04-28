e34f9a12d7d8d55c586a7c429672b3cd89688767
Kurt Stam <kstam@redhat.com>
2012-04-28 16:01:36 -0400

    SRAMP-5 adding JCR implementatation

--- s-ramp-repository/src/main/java/org/guvnor/sramp/repository/jcr/JCRRepository.java
+++ s-ramp-repository/src/main/java/org/guvnor/sramp/repository/jcr/JCRRepository.java
@@ -1,40 +1,65 @@
 package org.guvnor.sramp.repository.jcr;
 
+import static org.modeshape.jcr.api.observation.Event.NODE_SEQUENCED;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ServiceLoader;
 
-import javax.jcr.Credentials;
 import javax.jcr.LoginException;
 import javax.jcr.NoSuchWorkspaceException;
 import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
 import javax.jcr.RepositoryFactory;
 import javax.jcr.Session;
-import javax.jcr.SimpleCredentials;
+import javax.jcr.UnsupportedRepositoryOperationException;
+import javax.jcr.Workspace;
+
+import org.modeshape.jcr.api.AnonymousCredentials;
 
 public class JCRRepository {
 
-    private static String USER           = "s-ramp";
-    private static char[] PWD            = "s-ramp".toCharArray();
-    private static String WORKSPACE_NAME = "s-ramp";
+    //private static String USER           = "s-ramp";
+    //private static char[] PWD            = "s-ramp".toCharArray();
+    private static String WORKSPACE_NAME = "default";
     
     private static Repository repository = null;
+    private static SequencingListener listener = null;
     
-    public static Repository getInstance() throws RepositoryException {
+    private static Repository getInstance() throws RepositoryException {
         if (repository==null) {
             Map<String,String> parameters = new HashMap<String,String>();
+            String configUrl = Repository.class.getClassLoader().getResource("modeshape-config.json").toExternalForm();
+            parameters.put("org.modeshape.jcr.URL",configUrl);
             for (RepositoryFactory factory : ServiceLoader.load(RepositoryFactory.class)) {
                 repository = factory.getRepository(parameters);
                 if (repository != null) break;
             }
-            if (repository==null) throw new RepositoryException("Could not instantiate JCR Repository");
+            if (repository==null) throw new RepositoryException("ServiceLoader could not instantiate JCR Repository");
         }
+        getListener(); //create the listener
         return repository;
     }
     
+    public static SequencingListener getListener() throws UnsupportedRepositoryOperationException, LoginException, NoSuchWorkspaceException, RepositoryException {
+        if (listener == null) {
+            listener = new SequencingListener();
+            ((Workspace) getSession().getWorkspace()).getObservationManager().addEventListener(listener,
+                    NODE_SEQUENCED,
+                    null,
+                    true,
+                    null,
+                    null,
+                    false);
+        }
+        return listener;
+    }
+    
     public static Session getSession() throws LoginException, NoSuchWorkspaceException, RepositoryException {
-        Credentials cred = new SimpleCredentials(USER, PWD);
+        //Credentials cred = new SimpleCredentials(USER, PWD);
+        AnonymousCredentials cred = new AnonymousCredentials();
         return getInstance().login(cred, WORKSPACE_NAME);
     }
+    
+    
 }
--- s-ramp-repository/src/test/java/org/guvnor/sramp/repository/jcr/JCRPersistenceTest.java
+++ s-ramp-repository/src/test/java/org/guvnor/sramp/repository/jcr/JCRPersistenceTest.java
@@ -1,17 +1,95 @@
 package org.guvnor.sramp.repository.jcr;
 
+import java.io.InputStream;
+
+import javax.jcr.LoginException;
+import javax.jcr.NoSuchWorkspaceException;
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import junit.framework.Assert;
+
+import org.guvnor.sramp.repository.DerivedArtifactsCreationException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.modeshape.jcr.api.JcrTools;
+import org.s_ramp.xmlns._2010.s_ramp.XmlDocument;
+import org.s_ramp.xmlns._2010.s_ramp.XsdDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author <a href="mailto:kurt.stam@gmail.com">Kurt Stam</a>
- * @version $Revision: 1 $
  */
-public class XsdModelTest {
+public class JCRPersistenceTest {
+    
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+    static Session session = null;
+
+    @BeforeClass
+    public static void before() throws LoginException, NoSuchWorkspaceException, RepositoryException {
+        session = JCRRepository.getSession();
+    }
+    
+    @AfterClass
+    public static void after() {
+        session.logout();
+    }
     
     @Test
-    public void unmarshallCoreModelWorkspace() throws Exception {
-       
+    public void testSavePO_XSD() throws Exception {
+        
+        String artifactFileName = "PO.xsd";
+        InputStream POXsd = this.getClass().getResourceAsStream("/sample-files/xsd/" + artifactFileName);
+        
+        JCRPersistence persistence = new JCRPersistence();
+        String uuid = persistence.persistArtifact(artifactFileName, POXsd);
+        POXsd.close();
+        log.info("persisted PO.xsd to JCR, returned UUID=" + uuid);
+        Assert.assertNotNull(uuid);
+        XsdDocument xsdDocument = persistence.createDerivedArtifact(XsdDocument.class, artifactFileName);
+        String derivedUuid = persistence.persistDerivedArtifact(xsdDocument);
+        
+        //print out the derived node
+        Node derivedNode = session.getNodeByIdentifier(derivedUuid);
+        JcrTools tools = new JcrTools();
+        tools.printSubgraph(derivedNode);
+        
+        Assert.assertEquals(new Long(2376l), xsdDocument.getContentSize());
+        Assert.assertEquals("/s-ramp/xsd/XsdDocument/PO.xsd",derivedNode.getPath());
+        
+        System.out.println("XsdDocument = " + xsdDocument);
     }
-   
     
+    /**
+     * For now expecting a DerivedArtifactsCreationException since the ModeShape
+     * Sequencer for XML is not yet up deriving all the necessary artifacts.
+     * @throws Exception
+     */
+    @Test(expected=DerivedArtifactsCreationException.class)
+    public void testSavePO_XML() throws Exception {
+        
+        String artifactFileName = "PO.xml";
+        InputStream POXml = this.getClass().getResourceAsStream("/sample-files/xml/" + artifactFileName);
+        
+        JCRPersistence persistence = new JCRPersistence();
+        String uuid = persistence.persistArtifact(artifactFileName, POXml);
+        POXml.close();
+        log.info("persisted PO.xml to JCR, returned UUID=" + uuid);
+        Assert.assertNotNull(uuid);
+        XmlDocument xmlDocument = persistence.createDerivedArtifact(XmlDocument.class, artifactFileName);
+        String derivedUuid = persistence.persistDerivedArtifact(xmlDocument);
+        
+        //print out the derived node
+        Node derivedNode = session.getNodeByIdentifier(derivedUuid);
+        JcrTools tools = new JcrTools();
+        tools.printSubgraph(derivedNode);
+        
+        Assert.assertEquals(new Long(2376l), xmlDocument.getContentSize());
+        Assert.assertEquals("/s-ramp/xml/XmlDocument/PO.xml",derivedNode.getPath());
+        
+        System.out.println("XmlDocument = " + xmlDocument);
+    }
 }
