5c6606df333de20d062acde433b9cbde6df1cfcb
Kurt Stam <kstam@redhat.com>
2012-06-20 16:32:15 -0400

    adding jcr implementation

---
+++ s-ramp-repository/src/main/java/org/guvnor/sramp/repository/jcr/JCRConstants.java
@@ -0,0 +1,14 @@
+package org.guvnor.sramp.repository.jcr;
+
+
+public class JCRConstants {
+
+    public static final String OVERLORD         = "overlord";
+    public static final String SRAMP            = "sramp";
+    
+    public static final String OVERLORD_ARTIFACT         = OVERLORD + ":artifact";
+    public static final String OVERLORD_FILENAME         = OVERLORD + ":filename";
+    public static final String SRAMP_UUID                = SRAMP + ":uuid";
+    
+   
+}
