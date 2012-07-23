f0fd492f4d0592f76e8557e45c705d014e00bb01
Eric Wittmann <eric.wittmann@redhat.com>
2012-07-23 15:15:38 -0400

    Initial project structures for gwt and vaadin versions of the s-ramp-ui.

---
+++ s-ramp-ui-gwt/src/main/java/org/overlord/sramp/ui/client/Application.java
@@ -0,0 +1,46 @@
+package org.overlord.sramp.ui.client;
+
+import org.overlord.sramp.ui.client.places.HelloPlace;
+
+import com.google.gwt.activity.shared.ActivityManager;
+import com.google.gwt.activity.shared.ActivityMapper;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.place.shared.Place;
+import com.google.gwt.place.shared.PlaceController;
+import com.google.gwt.place.shared.PlaceHistoryHandler;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.web.bindery.event.shared.EventBus;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Application implements EntryPoint {
+
+	private Place defaultPlace = new HelloPlace();
+	private SimplePanel appWidget = new SimplePanel();
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		IClientFactory clientFactory = GWT.create(IClientFactory.class);
+		EventBus eventBus = clientFactory.getEventBus();
+		PlaceController placeController = clientFactory.getPlaceController();
+
+		// Start ActivityManager for the main widget with our ActivityMapper
+		ActivityMapper activityMapper = new ActivityMapperImpl(clientFactory);
+		ActivityManager activityManager = new ActivityManager(activityMapper, eventBus);
+		activityManager.setDisplay(appWidget);
+
+		// Start PlaceHistoryHandler with our PlaceHistoryMapper
+		IPlaceHistoryMapper historyMapper = GWT.create(IPlaceHistoryMapper.class);
+		PlaceHistoryHandler historyHandler = new PlaceHistoryHandler(historyMapper);
+		historyHandler.register(placeController, eventBus, defaultPlace);
+
+		RootPanel.get().add(appWidget);
+		// Goes to the place represented on URL else default place
+		historyHandler.handleCurrentHistory();
+	}
+}
