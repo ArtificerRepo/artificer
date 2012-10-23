4c0d442791f57902b46a79eaa7dc6910b0f69ff8
Kurt Stam <kstam@redhat.com>
2012-10-23 14:13:26 -0400

    SRAMP-9 adding scripts to setup brms connection

--- s-ramp-client/sramp.sh
+++ s-ramp-client/sramp.sh
@@ -20,15 +20,14 @@ echo "**************************************************************************
 if [ $# -lt 1 ]
 then
     echo "Not enough arguments were supplied. Usage:"
-    echo "sramp.sh -i  ....runs the S-RAMP interactive shell"
-    echo "sramp.sh brms [pkg_in_brms] [tag] [baseUrl] .....obtains 'pkg_in_brms' from brms/drools and uploads 
-                                                           to s-ramp"
+    echo "sramp.sh -i  ..................................................................runs the S-RAMP interactive shell"
+    echo "sramp.sh brms [pkg_in_brms] [tag] [baseUrl] [userId] [password] [srampUrl].....obtains 'pkg_in_brms' from brms/drools and uploads to s-ramp"
     exit 0
 fi
 
 if [ "$1" == "brms" ]
 then
     echo "Uploading brms package '$2' to S-RAMP..."
-    java -cp target/s-ramp-client-0.0.3-SNAPSHOT.jar:$(echo target/lib/*.jar | tr ' ' ':') org.overlord.sramp.client.brms.BrmsPkgToSramp $2 $3 $4
+    java -cp target/s-ramp-client-0.0.3-SNAPSHOT.jar:$(echo target/lib/*.jar | tr ' ' ':') org.overlord.sramp.client.brms.BrmsPkgToSramp $2 $3 $4 $5 $6 $7
     exit 0
 fi
