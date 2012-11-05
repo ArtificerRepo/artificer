99106a9022c88cd08525061c591127677b4e6ea8
EricWittmann <eric.wittmann@redhat.com>
2012-11-05 12:02:43 -0500

    Support for user defined types in the validating version of the s-ramp client.  Also added to the servicedocument.

---
+++ s-ramp-atom/src/main/java/org/overlord/sramp/atom/workspaces/UserDefinedWorkspace.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2011 JBoss Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.overlord.sramp.atom.workspaces;
+
+import org.jboss.resteasy.plugins.providers.atom.app.AppCollection;
+import org.overlord.sramp.ArtifactTypeEnum;
+import org.overlord.sramp.atom.MediaType;
+
+
+/**
+ * Models the S-RAMP user-defined workspace.
+ * @author eric.wittmann@redhat.com
+ */
+public class UserDefinedWorkspace extends AbstractWorkspace {
+
+	private static final long serialVersionUID = 1837991326302484187L;
+
+	/**
+	 * Constructor.
+	 * @param hrefBase
+	 */
+	public UserDefinedWorkspace(String hrefBase) {
+		super(hrefBase, "User Defined Model");
+	}
+
+	/**
+	 * @see org.overlord.sramp.atom.workspaces.AbstractWorkspace#configureWorkspace()
+	 */
+	@Override
+	protected void configureWorkspace() {
+        AppCollection userCollection = addCollection("/s-ramp/user", "User Defined Model Objects", MediaType.APPLICATION_OCTET_STREAM);
+
+        addTypeCategory(userCollection, ArtifactTypeEnum.UserDefinedArtifactType);
+    }
+}
