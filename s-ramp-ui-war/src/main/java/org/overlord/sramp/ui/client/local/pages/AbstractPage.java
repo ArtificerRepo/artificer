1b73511c2528e6f2caf68b69b0a38fe548104667
Eric Wittmann <eric.wittmann@redhat.com>
2013-03-07 14:00:33 -0500

    changes due to errai updates

--- s-ramp-ui-war/src/main/java/org/overlord/sramp/ui/client/local/pages/AbstractPage.java
+++ s-ramp-ui-war/src/main/java/org/overlord/sramp/ui/client/local/pages/AbstractPage.java
@@ -18,7 +18,7 @@ package org.overlord.sramp.ui.client.local.pages;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
-import org.jboss.errai.bus.client.framework.ClientMessageBus;
+import org.jboss.errai.bus.client.api.ClientMessageBus;
 import org.jboss.errai.enterprise.client.cdi.api.CDI;
 import org.jboss.errai.ui.nav.client.local.PageShowing;
 import org.jboss.errai.ui.nav.client.local.TransitionAnchor;
