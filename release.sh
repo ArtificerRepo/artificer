a074db8f8cb872b82a18204a73c4edf1804a03ff
Eric Wittmann <eric.wittmann@redhat.com>
2013-05-23 11:35:35 -0400

    fix typo in release.sh

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo ""
-echo "------------------------------------------------"
-echo "  Releasing S-RAMP (Community)
-echo "------------------------------------------------"
+echo "######################################"
+echo "  Releasing S-RAMP (Community)"
+echo "######################################"
 echo ""
 mvn -e --batch-mode clean release:prepare release:perform
 
