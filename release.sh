e97c568e9fcc77fc575f58b2b9a131bbf2e96878
Eric Wittmann <eric.wittmann@redhat.com>
2013-05-23 11:34:00 -0400

    changes to prep for release

---
+++ release.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo ""
+echo "------------------------------------------------"
+echo "  Releasing S-RAMP (Community)
+echo "------------------------------------------------"
+echo ""
+mvn -e --batch-mode clean release:prepare release:perform
+
