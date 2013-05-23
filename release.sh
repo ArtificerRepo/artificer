75e89cafd06d8b008c56cf93212445a51d624e1a
Eric Wittmann <eric.wittmann@redhat.com>
2013-05-23 11:52:14 -0400

    updated the release.sh script

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo ""
 echo "######################################"
-echo "  Releasing S-RAMP (Community)"
+echo "  Releasing S-RAMP UI (Community)"
 echo "######################################"
 echo ""
 mvn -e --batch-mode -DignoreSnapshots=true clean release:prepare release:perform
