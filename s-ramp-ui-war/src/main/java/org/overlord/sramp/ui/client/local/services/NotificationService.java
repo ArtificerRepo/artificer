07103f2ae104a63c0bb026c5b68349b5e06e6d39
Eric Wittmann <eric.wittmann@redhat.com>
2013-08-12 14:17:41 -0400

    i18n support is now context sensitive - based on the inbound HTTP request

--- s-ramp-ui-war/src/main/java/org/overlord/sramp/ui/client/local/services/NotificationService.java
+++ s-ramp-ui-war/src/main/java/org/overlord/sramp/ui/client/local/services/NotificationService.java
@@ -58,8 +58,6 @@ import com.google.gwt.user.client.ui.Widget;
 @ApplicationScoped
 public class NotificationService {
 
-    // TODO (i18n) find all consumers of this service to check for hard-coded strings that should be i18n'd
-
     @Inject
     private MessageBus bus;
     @Inject
