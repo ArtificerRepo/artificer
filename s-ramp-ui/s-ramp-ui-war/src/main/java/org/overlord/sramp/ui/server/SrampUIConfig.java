b34440fa592fea9be7b6e44cc523c79ea9fcbab9
Eric Wittmann <eric.wittmann@redhat.com>
2013-08-16 08:35:01 -0400

    UI now uses the common config project.
    https://issues.jboss.org/browse/SRAMP-215

--- s-ramp-ui/s-ramp-ui-war/src/main/java/org/overlord/sramp/ui/server/SrampUIConfig.java
+++ s-ramp-ui/s-ramp-ui-war/src/main/java/org/overlord/sramp/ui/server/SrampUIConfig.java
@@ -15,18 +15,10 @@
  */
 package org.overlord.sramp.ui.server;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import javax.inject.Singleton;
 
-import org.apache.commons.configuration.CompositeConfiguration;
 import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
-import org.apache.commons.configuration.SystemConfiguration;
-import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
+import org.overlord.commons.config.ConfigurationFactory;
 import org.overlord.sramp.ui.server.api.SrampApiClientAccessor;
 import org.overlord.sramp.ui.server.i18n.Messages;
 
@@ -50,85 +42,22 @@ public class SrampUIConfig {
     public static final String SRAMP_API_SAML_AUTH_SERVICE = "s-ramp-ui.atom-api.authentication.saml.service"; //$NON-NLS-1$
 
 
-    public static CompositeConfiguration config;
+    public static Configuration config;
     static {
-        SrampUIConfig.config = new CompositeConfiguration();
-        SrampUIConfig.config.addConfiguration(new SystemConfiguration());
-        String configFile = config.getString(SRAMP_UI_CONFIG_FILE_NAME);
-        Long refreshDelay = config.getLong(SRAMP_UI_CONFIG_FILE_REFRESH, 30000l);
-        URL url = findSrampUiConfig(configFile);
-        try {
-            if (url != null) {
-                PropertiesConfiguration propertiesConfiguration = new PropertiesConfiguration(url);
-                FileChangedReloadingStrategy fileChangedReloadingStrategy = new FileChangedReloadingStrategy();
-                fileChangedReloadingStrategy.setRefreshDelay(refreshDelay);
-                propertiesConfiguration.setReloadingStrategy(fileChangedReloadingStrategy);
-                config.addConfiguration(propertiesConfiguration);
-            }
-            SrampUIConfig.config.addConfiguration(new PropertiesConfiguration(SrampApiClientAccessor.class.getResource("/META-INF/config/org.overlord.sramp.ui.server.api.properties"))); //$NON-NLS-1$
-        } catch (ConfigurationException e) {
-            e.printStackTrace();
+        String configFile = System.getProperty(SRAMP_UI_CONFIG_FILE_NAME);
+        String refreshDelayStr = System.getProperty(SRAMP_UI_CONFIG_FILE_REFRESH);
+        Long refreshDelay = 5000l;
+        if (refreshDelayStr != null) {
+            refreshDelay = new Long(refreshDelayStr);
         }
+
+        config = ConfigurationFactory.createConfig(
+                configFile,
+                "sramp-ui.properties",
+                refreshDelay,
+                "/META-INF/config/org.overlord.sramp.ui.server.api.properties",
+                SrampApiClientAccessor.class);
         System.out.println(Messages.i18n.format("Config.Loaded", SrampUIConfig.config.getString(SRAMP_API_ENDPOINT))); //$NON-NLS-1$
-    }
-
-    /**
-     * Try to find the sramp-ui.properties configuration file.  This will look for the
-     * config file in a number of places, depending on the value for 'config file'
-     * found on the system properties.
-     * @param configFile
-     * @throws MalformedURLException
-     */
-    private static URL findSrampUiConfig(String configFile) {
-        try {
-            // If a config file was given (via system properties) then try to
-            // find it.  If not, then look for a 'standard' config file.
-            if (configFile != null) {
-                // Check on the classpath
-                URL fromClasspath = SrampUIConfig.class.getClassLoader().getResource(configFile);
-                if (fromClasspath != null)
-                    return fromClasspath;
-
-                // Check on the file system
-                File file = new File(configFile);
-                if (file.isFile())
-                    return file.toURI().toURL();
-            } else {
-                // Check the current user's home directory
-                String userHomeDir = System.getProperty("user.home"); //$NON-NLS-1$
-                if (userHomeDir != null) {
-                    File dirFile = new File(userHomeDir);
-                    if (dirFile.isDirectory()) {
-                        File cfile = new File(dirFile, "sramp-ui.properties"); //$NON-NLS-1$
-                        if (cfile.isFile())
-                            return cfile.toURI().toURL();
-                    }
-                }
-
-                // Next, check for JBoss
-                String jbossConfigDir = System.getProperty("jboss.server.config.dir"); //$NON-NLS-1$
-                if (jbossConfigDir != null) {
-                    File dirFile = new File(jbossConfigDir);
-                    if (dirFile.isDirectory()) {
-                        File cfile = new File(dirFile, "sramp-ui.properties"); //$NON-NLS-1$
-                        if (cfile.isFile())
-                            return cfile.toURI().toURL();
-                    }
-                }
-                String jbossConfigUrl = System.getProperty("jboss.server.config.url"); //$NON-NLS-1$
-                if (jbossConfigUrl != null) {
-                    File dirFile = new File(jbossConfigUrl);
-                    if (dirFile.isDirectory()) {
-                        File cfile = new File(dirFile, "sramp-ui.properties"); //$NON-NLS-1$
-                        if (cfile.isFile())
-                            return cfile.toURI().toURL();
-                    }
-                }
-            }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
     /**
