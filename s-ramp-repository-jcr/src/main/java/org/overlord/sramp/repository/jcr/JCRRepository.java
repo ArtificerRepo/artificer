4920979d638c3c10c7ad988cf8bca60a4c27ba77
Eric Wittmann <eric.wittmann@redhat.com>
2013-11-13 12:20:44 -0500

    initial tomcat 7 support

--- s-ramp-repository-jcr/src/main/java/org/overlord/sramp/repository/jcr/JCRRepository.java
+++ s-ramp-repository-jcr/src/main/java/org/overlord/sramp/repository/jcr/JCRRepository.java
@@ -15,8 +15,6 @@
  */
 package org.overlord.sramp.repository.jcr;
 
-import java.io.File;
-
 import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
 
@@ -59,31 +57,5 @@ public abstract class JCRRepository {
      * @return the JCR repository
      */
     public abstract Repository getRepo();
-
-	/**
-	 * Figures out what the current data directory is.  The data directory will be
-	 * different depending on where we're running.  In an application server this
-	 * code should strive to detect the app server specific data dir.
-	 */
-	public File determineRuntimeDataDir() {
-		// Our property takes precedent if present
-		String rootDataDir = System.getProperty("s-ramp.jcr.data.dir"); //$NON-NLS-1$
-		// Check for JBoss
-		if (rootDataDir == null) {
-			rootDataDir = System.getProperty("jboss.server.data.dir"); //$NON-NLS-1$
-		}
-		// Default to "data/"
-		if (rootDataDir == null) {
-			rootDataDir = "data"; //$NON-NLS-1$
-		}
-
-		File root = new File(rootDataDir);
-		File srampDataDir = new File(root, "s-ramp"); //$NON-NLS-1$
-		if (!srampDataDir.exists()) {
-			srampDataDir.mkdirs();
-		}
-
-		return srampDataDir;
-	}
 
 }
