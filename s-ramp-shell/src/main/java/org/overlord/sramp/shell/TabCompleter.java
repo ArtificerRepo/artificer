39b815946d2a91b7e572e1c48e2c9a1a0471db09
dvirgiln <david.virgil.naranjo@gmail.com>
2014-04-25 10:43:02 +0200

    SRAMP-408 Tab completion no longer works in s-ramp CLI

--- s-ramp-shell/src/main/java/org/overlord/sramp/shell/TabCompleter.java
+++ s-ramp-shell/src/main/java/org/overlord/sramp/shell/TabCompleter.java
@@ -26,6 +26,7 @@ import org.overlord.sramp.shell.api.Arguments;
 import org.overlord.sramp.shell.api.InvalidCommandArgumentException;
 import org.overlord.sramp.shell.api.ShellCommand;
 import org.overlord.sramp.shell.api.ShellContext;
+import org.overlord.sramp.shell.commands.CommandNotFoundCommand;
 
 /**
  * Implements tab completion for the interactive
@@ -34,19 +35,19 @@ import org.overlord.sramp.shell.api.ShellContext;
  */
 public class TabCompleter implements Completion {
 
-	private final ShellCommandFactory factory;
-	private final ShellContext context;
+    private final ShellCommandFactory factory;
+    private final ShellContext context;
 
-	/**
-	 * Constructor.
-	 * @param factory
-	 * @param context
-	 */
-	public TabCompleter(ShellCommandFactory factory, ShellContext context) {
-		this.factory = factory;
-		this.context = context;
-	}
-
+    /**
+     * Constructor.
+     *
+     * @param factory
+     * @param context
+     */
+    public TabCompleter(ShellCommandFactory factory, ShellContext context) {
+        this.factory = factory;
+        this.context = context;
+    }
 
     @Override
     public void complete(CompleteOperation completeOperation) {
@@ -56,81 +57,102 @@ public class TabCompleter implements Completion {
             for (String ns : factory.getNamespaces()) {
                 completeOperation.addCompletionCandidate(ns + ":"); //$NON-NLS-1$
             }
-        }
-
-        // Case 2 - a partial namespace has been typed - show all namespaces
-        // that match
-        if (!buffer.contains(":") && !buffer.contains(" ")) { //$NON-NLS-1$ //$NON-NLS-2$
-            for (String ns : factory.getNamespaces()) {
-                if (ns.startsWith(buffer)) {
-                    completeOperation.addCompletionCandidate(ns + ":"); //$NON-NLS-1$
-                }
+        } else {
+            // We check the first thing if what is introduced is a command or
+            // not.
+            // Check if what was introduced is a command itself
+            Arguments arguments = null;
+            try {
+                arguments = new Arguments(buffer, true);
+            } catch (InvalidCommandArgumentException e1) {
+                // should never happen...but if it does, just bail
             }
-            // If no namespaces matched, then try to match the default commands
-            if (completeOperation.getCompletionCandidates().isEmpty()) {
-                for (QName cmdName : factory.getCommandNames("s-ramp")) { //$NON-NLS-1$
-                    if (cmdName.getLocalPart().startsWith(buffer)) {
-                        completeOperation.addCompletionCandidate("s-ramp:" + cmdName.getLocalPart() + " "); //$NON-NLS-1$ //$NON-NLS-2$
-                    }
-                }
+            QName commandName = arguments.removeCommandName();
+            String lastArgument = null;
+            if (arguments.size() > 0 && !buffer.endsWith(" ")) { //$NON-NLS-1$
+                lastArgument = arguments.remove(arguments.size() - 1);
             }
-
-        }
-
-        // Case 3 - a namespace has been typed and we're waiting at the colon
-        if (buffer.endsWith(":") && !buffer.contains(" ")) { //$NON-NLS-1$ //$NON-NLS-2$
-            String ns = buffer.substring(0, buffer.length() - 1);
-            for (QName cmdName : factory.getCommandNames(ns)) {
-                completeOperation.addCompletionCandidate(cmdName.getLocalPart() + " "); //$NON-NLS-1$
+            ShellCommand command = null;
+            try {
+                command = factory.createCommand(commandName);
+            } catch (Exception e) {
             }
-
-        }
-
-        // Case 4 - a partial command has been typed - show all command names
-        // that match
-        if (buffer.contains(":") && !buffer.endsWith(":") && !buffer.contains(" ")) { //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-            int colonIdx = buffer.indexOf(':');
-            String ns = buffer.substring(0, colonIdx);
-            String name = buffer.substring(colonIdx + 1);
-            for (QName cmdName : factory.getCommandNames(ns)) {
-                if (cmdName.getLocalPart().startsWith(name)) {
-                    completeOperation.addCompletionCandidate(cmdName.getLocalPart() + " "); //$NON-NLS-1$
-                }
-            }
-        }
-
-        // Case 5 - a full command name has been entered, delegate further
-        // procesing
-        // to the specific command
-
-        Arguments arguments = null;
-        try {
-            arguments = new Arguments(buffer, true);
-        } catch (InvalidCommandArgumentException e1) {
-            // should never happen...but if it does, just bail
-        }
-        QName commandName = arguments.removeCommandName();
-        String lastArgument = null;
-        if (arguments.size() > 0 && !buffer.endsWith(" ")) { //$NON-NLS-1$
-            lastArgument = arguments.remove(arguments.size() - 1);
-        }
-        try {
-            ShellCommand command = factory.createCommand(commandName);
-            if (command != null) {
+            // In case it is a command then we print the tabCompletion specific
+            // of the command
+            if (command != null && !(command instanceof CommandNotFoundCommand)) {
                 command.setContext(this.context);
                 command.setArguments(arguments);
 
                 List<CharSequence> list = new ArrayList<CharSequence>();
                 command.tabCompletion(lastArgument, list);
                 if (!list.isEmpty()) {
-                    for (CharSequence sequence : list) {
-                        completeOperation.addCompletionCandidate(sequence.toString());
+                    // In case the tab completion return just one result it is
+                    // printed the previous buffer plus the argument
+                    if (list.size() == 1) {
+                        completeOperation.addCompletionCandidate(buffer.trim() + " " + list.get(0));
+                    } else {
+                        // In case the result of the command tab completion
+                        // contains more than one result (like the
+                        // FileNameCompleter
+                        for (CharSequence sequence : list) {
+                            completeOperation.addCompletionCandidate(sequence.toString());
+                        }
+                    }
+
+                }
+            } else if (!buffer.contains(":") && !buffer.contains(" ")) { //$NON-NLS-1$ //$NON-NLS-2$
+                // Case 2 - a partial namespace has been typed - show all
+                // namespaces
+                // that match
+                for (String ns : factory.getNamespaces()) {
+                    if (ns.startsWith(buffer)) {
+                        completeOperation.addCompletionCandidate(ns + ":"); //$NON-NLS-1$
+                    }
+                }
+                // If no namespaces matched, then try to match the default
+                // commands
+
+                if (completeOperation.getCompletionCandidates().isEmpty()) {
+                    List<String> toBeAdded = new ArrayList<String>();
+                    for (QName cmdName : factory.getCommandNames("s-ramp")) { //$NON-NLS-1$
+                        if (cmdName.getLocalPart().startsWith(buffer)) {
+                            toBeAdded.add("s-ramp:" + cmdName.getLocalPart() + " ");
+
+                        }
+                    }
+                    if (toBeAdded.size() == 1) {
+                        String cmdName = toBeAdded.get(0).substring(toBeAdded.get(0).indexOf(":") + 1);
+                        completeOperation.addCompletionCandidate(cmdName.trim()); //$NON-NLS-1$ //$NON-NLS-2$
+                    }
+                } else if (completeOperation.getCompletionCandidates().size() == 1) {
+                    completeOperation.doAppendSeparator(false);
+                }
+
+            }
+            // Case 3 - a namespace has been typed and we're waiting at the
+            // colon
+            else if (buffer.endsWith(":") && !buffer.contains(" ")) { //$NON-NLS-1$ //$NON-NLS-2$
+                String ns = buffer.substring(0, buffer.length() - 1);
+                for (QName cmdName : factory.getCommandNames(ns)) {
+                    completeOperation.addCompletionCandidate(cmdName.getLocalPart() + " "); //$NON-NLS-1$
+                }
+
+            }
+            // Case 4 - a partial command has been typed - show all command
+            // names
+            // that match
+            else if (buffer.contains(":") && !buffer.endsWith(":") && !buffer.contains(" ")) { //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                int colonIdx = buffer.indexOf(':');
+                String ns = buffer.substring(0, colonIdx);
+                String name = buffer.substring(colonIdx + 1);
+                for (QName cmdName : factory.getCommandNames(ns)) {
+                    if (cmdName.getLocalPart().startsWith(name)) {
+                        completeOperation.addCompletionCandidate(cmdName.getNamespaceURI()+":"+cmdName.getLocalPart()); //$NON-NLS-1$
                     }
                 }
             }
-        } catch (Exception e) {
         }
 
-    }
 
+    }
 }
