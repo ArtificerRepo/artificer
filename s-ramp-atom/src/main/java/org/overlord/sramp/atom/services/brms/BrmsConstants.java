e98d4b076f02d952b0ed3e2c601c6dfcbf5cd175
Brett Meyer <brett@3riverdev.com>
2014-07-15 15:04:54 -0400

    SRAMP-431 removed a few brms leftovers

--- s-ramp-atom/src/main/java/org/overlord/sramp/atom/services/brms/BrmsConstants.java
+++
@@ -1,7 +0,0 @@
-package org.overlord.sramp.atom.services.brms;
-
-public interface BrmsConstants {
-
-    String BRMS_PKG_DOCUMENT   = "BrmsPkgDocument"; //$NON-NLS-1$
-    String ASSET_INFO_XML      = "AssetInfoXML"; //$NON-NLS-1$
-}
