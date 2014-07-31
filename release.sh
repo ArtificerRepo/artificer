3d0a32557467f806457723ddc6e8d1aed7569431
Brett Meyer <brett@3riverdev.com>
2014-07-31 09:42:44 -0400

    new release.sh script

--- release.sh
+++ release.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 echo ""
 echo "######################################"
-echo "  Releasing S-RAMP (Community)"
+echo "  Releasing Overlord S-RAMP"
 echo "######################################"
 echo ""
 read -p "Release Version: " RELEASE_VERSION
 read -p "New Development Version: " DEV_VERSION
-mvn -e --batch-mode -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEV_VERSION clean release:prepare release:perform
+
+mvn versions:set -DnewVersion=$RELEASE_VERSION
+find . -name '*.versionsBackup' -exec rm -f {} \;
+git add .
+git commit -m "Prepare for release $RELEASE_VERSION"
+git push origin master
+
+mvn clean install
+
+git tag -a -m "Tagging release $RELEASE_VERSION" s-ramp-$RELEASE_VERSION
+git push origin s-ramp-$RELEASE_VERSION
+
+mvn deploy
+
+mvn versions:set -DnewVersion=$DEV_VERSION
+find . -name '*.versionsBackup' -exec rm -f {} \;
+git add .
+git commit -m "Update to next development version: $DEV_VERSION"
+git push origin master
 
