90ee32e6a34f85686747327229d87ab0f572670f
Brett Meyer <brett@3riverdev.com>
2014-10-03 16:06:02 -0400

    corrected s-ramp-doc profiles and release.sh

--- release.sh
+++ release.sh
@@ -10,21 +10,22 @@ read -p "Full path to your private key: " KEYFILE
 
 mvn versions:set -DnewVersion=$RELEASE_VERSION
 find . -name '*.versionsBackup' -exec rm -f {} \;
+
+# sanity check
+mvn clean install -P generate-docs
+
 git add .
 git commit -m "Prepare for release $RELEASE_VERSION"
 git push origin master
 
-mvn clean install -P generate-docs
-
 git tag -a -m "Tagging release $RELEASE_VERSION" s-ramp-$RELEASE_VERSION
 git push origin s-ramp-$RELEASE_VERSION
 
-mvn deploy -P generate-docs -Dkeyfile=$KEYFILE
+mvn deploy -Pgenerate-docs,upload-docs -Dkeyfile=$KEYFILE
+scp s-ramp-distro/assembly/target/s-ramp-$RELEASE_VERSION.zip overlord@filemgmt.jboss.org:/downloads_htdocs/overlord/sramp
 
 mvn versions:set -DnewVersion=$DEV_VERSION
 find . -name '*.versionsBackup' -exec rm -f {} \;
 git add .
 git commit -m "Update to next development version: $DEV_VERSION"
 git push origin master
-
-scp s-ramp-distro/assembly/target/s-ramp-$RELEASE_VERSION.zip overlord@filemgmt.jboss.org:/downloads_htdocs/overlord/sramp
