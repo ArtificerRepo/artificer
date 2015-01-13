d8312db3b5e74605557cdf5f27ade9f949005663
bouskaJ <xbouska1@mail.muni.cz>
2015-01-13 14:48:46 +0100

    [s-ramp-ide] Initial implementation

---
+++ s-ramp-ide/srampRepositoryBrowser/src/main/java/cz/muni/fi/srampRepositoryBrowser/manager/ServiceFailureException.java
@@ -0,0 +1,23 @@
+package cz.muni.fi.srampRepositoryBrowser.manager;
+
+/**
+ * service failture exception
+ * 
+ * @author Jan Bouska
+ * 
+ */
+public class ServiceFailureException extends RuntimeException {
+
+	public ServiceFailureException(String msg) {
+		super(msg);
+	}
+
+	public ServiceFailureException(Throwable cause) {
+		super(cause);
+	}
+
+	public ServiceFailureException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
