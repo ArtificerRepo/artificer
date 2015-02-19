4542a42dbf007dded9b65a012625b16813867f3b
Brett Meyer <brett@3riverdev.com>
2015-02-19 11:38:17 -0500

    corrected Errai dependencies

--- ui/src/main/java/org/artificer/ui/client/local/pages/AbstractPage.java
+++ ui/src/main/java/org/artificer/ui/client/local/pages/AbstractPage.java
@@ -19,7 +19,6 @@ import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.jboss.errai.bus.client.api.ClientMessageBus;
-import org.jboss.errai.enterprise.client.cdi.api.CDI;
 import org.jboss.errai.ui.nav.client.local.PageShowing;
 import org.artificer.ui.client.local.util.ArtificerJS;
 
@@ -54,25 +53,11 @@ public abstract class AbstractPage extends Composite {
     /**
      * Called when a page is about to be shown (either when the app is first loaded or
      * when navigating TO this page from another).
-     */
-    @PageShowing
-    private final void _onPageShowing() {
-        // Do initial page loading work, but do it as a post-init task
-        // of the errai bus so that all endpoints are ready.  This
-        // is only necessary on initial app load, but it doesn't hurt
-        // to always do it.
-        CDI.addPostInitTask(new Runnable() {
-            @Override
-            public void run() {
-                onPageShowing();
-            }
-        });
-    }
-
-    /**
+     *
      * Subclasses can implement this to do any work they need done when the page
      * is about to be shown.
      */
+    @PageShowing
     protected void onPageShowing() {
     }
 
