513b5b3978b0751a11eaefd1273df5927aa1fc13
Brett Meyer <brett@3riverdev.com>
2015-06-11 22:49:25 -0400

    ARTIF-703 Auto install DDL if the tables don't exist

--- repository/hibernate/src/main/java/org/artificer/repository/hibernate/HibernateUtil.java
+++ repository/hibernate/src/main/java/org/artificer/repository/hibernate/HibernateUtil.java
@@ -17,6 +17,7 @@ package org.artificer.repository.hibernate;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.commons.io.IOUtils;
 import org.artificer.common.ArtificerConfig;
 import org.artificer.common.ArtificerException;
 import org.artificer.common.error.ArtificerNotFoundException;
@@ -26,14 +27,20 @@ import org.artificer.repository.hibernate.entity.ArtificerArtifact;
 import org.artificer.repository.hibernate.entity.ArtificerStoredQuery;
 import org.hibernate.Hibernate;
 import org.hibernate.ejb.HibernatePersistence;
+import org.hibernate.engine.spi.SessionImplementor;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
-import java.io.PrintWriter;
+import java.io.InputStream;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Locale;
 import java.util.Map;
-import java.util.Properties;
 
 /**
  * @author Brett Meyer.
@@ -88,43 +95,112 @@ public class HibernateUtil {
         protected abstract T doExecute(EntityManager entityManager) throws Exception;
     }
 
-    private static EntityManager entityManager() {
+    private static EntityManager entityManager() throws Exception {
         if (entityManagerFactory == null) {
             // Pass in all hibernate.* settings from artificer.properties
             Map<String, Object> properties = ArtificerConfig.getConfigProperties("hibernate");
 
-            // If a connection is used, we *cannot* rely on Hibernate's built-in connection pool.  Instead,
-            // automatically set up HikariCP.
             if (properties.containsKey("hibernate.connection.url")) {
-                String connectionUrl = (String) properties.remove("hibernate.connection.url");
-                String username = (String) properties.remove("hibernate.connection.username");
-                String password = (String) properties.remove("hibernate.connection.password");
-
-                HikariConfig hikariConfig = new HikariConfig();
-                hikariConfig.setJdbcUrl(connectionUrl);
-                hikariConfig.setUsername(username);
-                hikariConfig.setPassword(password);
-
-                // In case we're using MySQL, these settings are recommended by HikariCP:
-                hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
-                hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
-                hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-                hikariConfig.addDataSourceProperty("useServerPrepStmts", "true");
-
-                String dialect = (String) properties.get("hibernate.dialect");
-                if (dialect != null && dialect.contains("PostgreSQL")) {
-                    // The JDBC jar verion in the IP BOM does not support Connection.isValid(), so need to use this:
-                    hikariConfig.setConnectionTestQuery("SELECT 1");
-                }
-
-                HikariDataSource hikariDataSource = new HikariDataSource(hikariConfig);
-
-                properties.put("hibernate.connection.datasource", hikariDataSource);
+                // If a connection is used, we *cannot* rely on Hibernate's built-in connection pool.  Instead,
+                // automatically set up HikariCP.
+                initHikariCP(properties);
             }
 
             entityManagerFactory = new HibernatePersistence().createEntityManagerFactory(persistenceUnit, properties);
+
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            initDDL(entityManager, properties);
+            return entityManager;
+        } else {
+            return entityManagerFactory.createEntityManager();
         }
-        return entityManagerFactory.createEntityManager();
+    }
+
+    private static void initHikariCP(Map<String, Object> properties) {
+        String connectionUrl = (String) properties.remove("hibernate.connection.url");
+        String username = (String) properties.remove("hibernate.connection.username");
+        String password = (String) properties.remove("hibernate.connection.password");
+
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(connectionUrl);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
+
+        // In case we're using MySQL, these settings are recommended by HikariCP:
+        hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
+        hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
+        hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        hikariConfig.addDataSourceProperty("useServerPrepStmts", "true");
+
+        String dialect = (String) properties.get("hibernate.dialect");
+        if (dialect != null && dialect.contains("PostgreSQL")) {
+            // The JDBC jar verion in the IP BOM does not support Connection.isValid(), so need to use this:
+            hikariConfig.setConnectionTestQuery("SELECT 1");
+        }
+
+        HikariDataSource hikariDataSource = new HikariDataSource(hikariConfig);
+
+        properties.put("hibernate.connection.datasource", hikariDataSource);
+    }
+
+    private static void initDDL(EntityManager entityManager, Map<String, Object> properties) throws Exception {
+        // If the DDL is not already installed in the DB, automatically do it on first use.
+        SessionImplementor session  = (SessionImplementor) entityManager.getDelegate();
+        Connection connection = session.connection();
+
+        if (!hasTables(connection)) {
+            // our tables don't exist -- create them
+            String dialect = (String) properties.get("hibernate.dialect");
+            if (dialect != null) {
+                String ddlFile;
+                if (dialect.contains("PostgreSQL")) {
+                    ddlFile = "postgres9.sql";
+                } else if (dialect.contains("MySQL")) {
+                    ddlFile = "mysql5.sql";
+                } else if (dialect.contains("Oracle")) {
+                    ddlFile = "oracle10.sql";
+                } else if (dialect.contains("SQLServer")) {
+                    ddlFile = "mssql2012.sql";
+                } else if (dialect.contains("DB2")) {
+                    ddlFile = "db2.sql";
+                } else {
+                    ddlFile = "h2.sql";
+                }
+
+                Statement statement = null;
+                try {
+                    URL url = HibernateUtil.class.getClassLoader().getResource("ddl/" + ddlFile);
+                    String ddl = IOUtils.toString(url);
+
+                    statement = connection.createStatement();
+                    statement.executeUpdate(ddl);
+                } finally {
+                    if (statement != null) {
+                        statement.close();
+                    }
+                    // do *not* close the connection -- it will still be used by this instance of the EntityManager
+                }
+            }
+        }
+    }
+
+    private static boolean hasTables(Connection connection) throws Exception {
+        DatabaseMetaData metadata = connection.getMetaData();
+
+        // check if "ArtificerArtifact" table exists
+        ResultSet tables = metadata.getTables(null, null, ArtificerArtifact.class.getSimpleName(), null);
+        if (tables.next()) {
+            return true;
+        }
+
+        // also need to check all caps (thanks, Oracle)
+        tables = metadata.getTables(null, null, ArtificerArtifact.class.getSimpleName().toUpperCase(Locale.ROOT), null);
+        if (tables.next()) {
+            return true;
+        }
+
+        // otherwise, nope
+        return false;
     }
 
     public static ArtificerArtifact getArtifact(String uuid, EntityManager entityManager, boolean fullFetch) throws ArtificerException {
